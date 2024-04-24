e6052b8f8a74b4c2d7758be6b22b2ede781163a9
Charles li <lililililock@gmail.com>
2024-04-24 08:24:04 +0800

    add point structure

--- include/RGBFrames.hpp
+++ include/RGBFrames.hpp
@@ -4,12 +4,33 @@
 #include <memory>
 #include <unordered_map>
 #include "FrameData.hpp"
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
 
-struct RGBPoint
+// struct RGBPoint
+// {
+//     float x, y, z;
+//     std::unordered_map<int, std::shared_ptr<FrameData>> frames;
+// };
+
+// Define a new point type that inherits from pcl::PointXYZRGB and adds a new float field
+struct PointXYZRGBMask : public pcl::PointXYZRGB
 {
-    float x, y, z;
-    std::unordered_map<int, std::shared_ptr<FrameData>> frames;
-};
+  float segmentMask;
+  
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW   // Ensure proper alignment for performance reasons
+} EIGEN_ALIGN16;  // Enforces SSE padding for correct memory alignment
+
+// Register the new point type with Point Cloud Library
+POINT_CLOUD_REGISTER_POINT_STRUCT(PointXYZRGBMask,
+                                  (float, x, x)
+                                  (float, y, y)
+                                  (float, z, z)
+                                  (std::uint8_t, r, r)
+                                  (std::uint8_t, g, g)
+                                  (std::uint8_t, b, b)
+                                  (float, segmentMask, segmentMask)
+)
 
 class RGBFrames
 {
